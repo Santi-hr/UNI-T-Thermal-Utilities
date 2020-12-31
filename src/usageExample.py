622030202effd0aac82694f75075cd47deb38ee5
Santi H <san.2005@hotmail.com>
2020-12-31 18:24:43 +0100

    Added usage example

---
+++ src/usageExample.py
@@ -0,0 +1,21 @@
+import matplotlib.pyplot as plt
+from src import uniTThermalImage
+
+print("UNI T Thermal Image Extractor Demo")
+
+# Load image
+filename = "../examples/samples/IMG_Typical.bmp"
+obj_uti = uniTThermalImage.UniTThermalImage()
+obj_uti.init_from_image(filename)
+
+# Print temperature data
+print("Min temp: %f º%c" % (obj_uti.temp_min, obj_uti.temp_units))
+print("Max temp: %f º%c" % (obj_uti.temp_max, obj_uti.temp_units))
+
+# Get temperature of a point
+point = (100, 100)
+print("Temp in", point, ":", obj_uti.temp_array_np[point], obj_uti.temp_units)
+
+# Plot clean image
+plt.imshow(obj_uti.raw_image_rgb_np)
+plt.show()
