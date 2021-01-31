898a4c579a60d4787f721ce9ca6fb76a95795148
Santi H <san.2005@hotmail.com>
2021-01-31 15:16:28 +0100

    Refactor code

--- src/uniTThermalImage.py
+++ src/uniTThermalImage.py
@@ -57,25 +57,25 @@ class UniTThermalImage:
         # Read file and save file modification date
         with open(path_in, "rb") as file:
             self.file_bytes = file.read()
-        self._get_file_time_os(path_in)
+        self.__get_file_time_os(path_in)
 
         # Read BMP header
-        self._get_bmp_header()
+        self.__get_bmp_header()
         # Get grayscale image after .bmp
-        byte_offset = self._extract_grayscale_img()
+        byte_offset = self.__extract_grayscale_img()
 
         # Get colorbar after grayscale image
-        byte_offset = self._extract_colorbar(byte_offset)
+        byte_offset = self.__extract_colorbar(byte_offset)
 
         # Get embedded temperature after colorbar
-        byte_offset = self._extract_temp_data(byte_offset)
+        byte_offset = self.__extract_temp_data(byte_offset)
 
         # Get embedded timestamp after embedded data
-        self._extract_file_time(byte_offset)
+        self.__extract_file_time(byte_offset)
 
         # Set internal variables using the loaded data
-        self._set_rgb_image()
-        self._set_temp_matrix()
+        self.__set_rgb_image()
+        self.__set_temp_matrix()
 
         self.flag_initialized = True
 
@@ -93,16 +93,16 @@ class UniTThermalImage:
                          self.temp_min_pos_w, self.temp_min_pos_h, self.temp_max_pos_w, self.temp_max_pos_h,
                          self.img_datetime.strftime("%Y-%m-%d %H:%M:%S")]
 
-            file.write(self._csv_str_line_formatter(header_data, flag_es))
-            file.write(self._csv_str_line_formatter(data_data, flag_es))
+            file.write(self.__csv_str_line_formatter(header_data, flag_es))
+            file.write(self.__csv_str_line_formatter(data_data, flag_es))
 
             header_data_img = ["Pixel temperatures", "Down Y (Height)", "Right X (Width)"]
-            file.write(self._csv_str_line_formatter(header_data_img, flag_es))
+            file.write(self.__csv_str_line_formatter(header_data_img, flag_es))
             for idx_h in range(len(self.temp_array_np)):
                 temp_list = []
                 for idx_w in range(len(self.temp_array_np[idx_h])):
                     temp_list.append(self.temp_array_np[idx_h, idx_w])
-                file.write(self._csv_str_line_formatter(temp_list, flag_es))
+                file.write(self.__csv_str_line_formatter(temp_list, flag_es))
 
     def export_bmp(self):
         """Exports a version of the input image without its overlay. It keeps the embedded data"""
@@ -120,7 +120,7 @@ class UniTThermalImage:
 
         # Add extra data at the end of file.
         # Only done before exporting to avoid converting from bytes to list multiple times
-        self._serialize_additional_data(output_bytes)
+        self.__serialize_additional_data(output_bytes)
 
         # Write to file
         output_path = self.output_folder / (self.filename + self.bmp_suffix)
@@ -139,7 +139,7 @@ class UniTThermalImage:
             raise OSError("Output folder %s is not a valid directoy" % folder_path)
         self.output_folder = folder_path
 
-    def _csv_str_line_formatter(self, list_in, flag_es):
+    def __csv_str_line_formatter(self, list_in, flag_es):
         """Helper to format the strings to be written on the csv
 
         :param list_in: List that represent one csv line
@@ -154,7 +154,7 @@ class UniTThermalImage:
             decimal_sep = ","
         return (delimiter.join(converted_list).replace(".", decimal_sep)) + '\n'
 
-    def _read_int32(self, ini):
+    def __read_int32(self, ini):
         """Gets a 32 signed bit variable stored as Little-Endian in the file bytes
 
         :param ini: First byte of the variable to extract
@@ -167,7 +167,7 @@ class UniTThermalImage:
             int_return = ~(int_return ^ 0xFFFFFFFF)
         return int_return
 
-    def _read_int16(self, ini):
+    def __read_int16(self, ini):
         """Gets a 16 signed bit variable stored as Little-Endian in the file bytes
 
         :param ini: First byte of the variable to extract
@@ -179,32 +179,32 @@ class UniTThermalImage:
             int_return = ~(int_return ^ 0xFFFF)
         return int_return
 
-    def _get_file_time_os(self, path_in):
+    def __get_file_time_os(self, path_in):
         """Gets the modification time of the file. For a raw bmp this is also the image capture time.
 
         :param path_in: Libpath object to the file being loaded
         """
         self.img_datetime = datetime.fromtimestamp(path_in.stat().st_mtime)
 
-    def _get_bmp_header(self):
+    def __get_bmp_header(self):
         """Gets data from an standard bmp header into a class dictionary"""
-        self.bmp_header['file_size'] = self._read_int32(2)
-        self.bmp_header['reserved_1'] = self._read_int16(6)
-        self.bmp_header['reserved_2'] = self._read_int16(8)
-        self.bmp_header['data_start_byte'] = self._read_int32(10)
-        self.bmp_header['bmp_header_size'] = self._read_int32(14)
-        self.bmp_header['img_width_px'] = self._read_int32(18)
-        self.bmp_header['img_height_px'] = self._read_int32(22)
-        self.bmp_header['plane_num'] = self._read_int16(26)
-        self.bmp_header['bits_per_px'] = self._read_int16(28)
-        self.bmp_header['compression'] = self._read_int32(30)
-        self.bmp_header['img_size'] = self._read_int32(34)
-        self.bmp_header['horizontal_res'] = self._read_int32(38)
-        self.bmp_header['vertical_res'] = self._read_int32(42)
-        self.bmp_header['color_table_size'] = self._read_int32(46)
-        self.bmp_header['important_colors'] = self._read_int32(50)
+        self.bmp_header['file_size'] = self.__read_int32(2)
+        self.bmp_header['reserved_1'] = self.__read_int16(6)
+        self.bmp_header['reserved_2'] = self.__read_int16(8)
+        self.bmp_header['data_start_byte'] = self.__read_int32(10)
+        self.bmp_header['bmp_header_size'] = self.__read_int32(14)
+        self.bmp_header['img_width_px'] = self.__read_int32(18)
+        self.bmp_header['img_height_px'] = self.__read_int32(22)
+        self.bmp_header['plane_num'] = self.__read_int16(26)
+        self.bmp_header['bits_per_px'] = self.__read_int16(28)
+        self.bmp_header['compression'] = self.__read_int32(30)
+        self.bmp_header['img_size'] = self.__read_int32(34)
+        self.bmp_header['horizontal_res'] = self.__read_int32(38)
+        self.bmp_header['vertical_res'] = self.__read_int32(42)
+        self.bmp_header['color_table_size'] = self.__read_int32(46)
+        self.bmp_header['important_colors'] = self.__read_int32(50)
 
-    def _extract_grayscale_img(self):
+    def __extract_grayscale_img(self):
         """Extracts the raw thermal image embedded after the .bmp data to a numpy array.
         The image depth is 1 byte per pixel [0, 254]
 
@@ -217,7 +217,7 @@ class UniTThermalImage:
 
         return byte_image_end
 
-    def _extract_colorbar(self, byte_index_input):
+    def __extract_colorbar(self, byte_index_input):
         """Extracts the colorbar embedded in the .bmp data, after grayscale img, to a numpy array
 
         :param byte_index_input: Starting byte of the data segment
@@ -228,7 +228,7 @@ class UniTThermalImage:
         counter_colorbar = 0
         for i in range(byte_index_input, byte_index_input + colorbar_size, bytes_per_color):
             # Deserialize int16
-            color_raw = self._read_int16(i)
+            color_raw = self.__read_int16(i)
             # 5 bits red, 6 bits green, 5 bits blue
             color_r_raw = (color_raw & 0xF800) >> 11
             color_g_raw = (color_raw & 0x7E0) >> 5
@@ -243,7 +243,7 @@ class UniTThermalImage:
         byte_offset = byte_index_input + colorbar_size
         return byte_offset
 
-    def _extract_temp_data(self, byte_index_input):
+    def __extract_temp_data(self, byte_index_input):
         """Extracts the embedded temperature information in the .bmp data, after colorbar, to variables
 
         :param byte_index_input: Starting byte of the data segment
@@ -253,25 +253,25 @@ class UniTThermalImage:
         else:
             self.temp_units = 'F'
 
-        self.temp_max = self._read_int16(byte_index_input + 1) / 10.0
-        self.temp_min = self._read_int16(byte_index_input + 3) / 10.0
+        self.temp_max = self.__read_int16(byte_index_input + 1) / 10.0
+        self.temp_min = self.__read_int16(byte_index_input + 3) / 10.0
         # Byte 5, unknown. Always 255
         # Byte 6, unknown. Always 0
-        self.temp_center = self._read_int16(byte_index_input + 7) / 10.0
+        self.temp_center = self.__read_int16(byte_index_input + 7) / 10.0
         self.emissivity = self.file_bytes[byte_index_input + 9] / 100.0
         # Byte 10, unknown. Always 6
         # Byte 11, 12 & 13, unknown. Always 0
-        self.temp_min_pos_w = self._read_int16(byte_index_input + 14)
-        self.temp_min_pos_h = self._read_int16(byte_index_input + 16)
-        self.temp_max_pos_w = self._read_int16(byte_index_input + 18)
-        self.temp_max_pos_h = self._read_int16(byte_index_input + 20)
-        self.temp_center_pos_w = self._read_int16(byte_index_input + 22)
-        self.temp_center_pos_h = self._read_int16(byte_index_input + 24)
+        self.temp_min_pos_w = self.__read_int16(byte_index_input + 14)
+        self.temp_min_pos_h = self.__read_int16(byte_index_input + 16)
+        self.temp_max_pos_w = self.__read_int16(byte_index_input + 18)
+        self.temp_max_pos_h = self.__read_int16(byte_index_input + 20)
+        self.temp_center_pos_w = self.__read_int16(byte_index_input + 22)
+        self.temp_center_pos_h = self.__read_int16(byte_index_input + 24)
 
         byte_offset = byte_index_input + 26
         return byte_offset
 
-    def _extract_file_time(self, byte_index_input):
+    def __extract_file_time(self, byte_index_input):
         """Extracts the capture timestamp from the .bmp data (If existant)
 
         :param byte_index_input: Starting byte of the data segment
@@ -279,16 +279,16 @@ class UniTThermalImage:
         # Only existent if the bmp is generated from this script
         # If there is no more data in the file it means that it is raw from the thermal camera
         if byte_index_input != len(self.file_bytes):
-            self.img_datetime = datetime.fromtimestamp(self._read_int32(byte_index_input))
+            self.img_datetime = datetime.fromtimestamp(self.__read_int32(byte_index_input))
 
-    def _serialize_additional_data(self, output_bytes):
+    def __serialize_additional_data(self, output_bytes):
         """Adds additional data to the end of the output bytes
 
         :param output_bytes: Reference to the list with the file bytes converted into list
         """
-        self._serialize_file_time(output_bytes)
+        self.__serialize_file_time(output_bytes)
 
-    def _serialize_file_time(self, output_bytes):
+    def __serialize_file_time(self, output_bytes):
         """Appends the timestamp of the capture at the end of the file bytes
 
         :param output_bytes: Reference to the list with the file bytes converted into list
@@ -299,12 +299,12 @@ class UniTThermalImage:
         output_bytes.append((timestamp >> 16) & 0xFF)
         output_bytes.append((timestamp >> 24) & 0xFF)
 
-    def _set_rgb_image(self):
+    def __set_rgb_image(self):
         """Applies colorbar to raw thermal image to generate a clean rgb version of the BMP image"""
         # As the raw images are integers between 0 and 255 we can use direct numpy indexing
         self.raw_img_rgb_np = self.colorbar_rgb_np[self.raw_img_np]
 
-    def _set_temp_matrix(self):
+    def __set_temp_matrix(self):
         """Calculates the temperature of each pixel in the raw thermal image"""
         self.temp_array_np = self.temp_min + (self.temp_max - self.temp_min) * (self.raw_img_np / 254.0)
 
